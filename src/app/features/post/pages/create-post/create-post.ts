482edd0017e9f7597070cdbb8b9dfb7c3d7e753e
alexc83 <alex.cruce@gmail.com>
2026-04-05 01:55:37 -0500

    feat: add create post form plus validation

--- src/app/features/post/pages/create-post/create-post.ts
+++ src/app/features/post/pages/create-post/create-post.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-import { Button } from "../../../../shared/button/button";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
-  imports: [Button],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-post.html',
   styleUrl: './create-post.css',
 })
-export class CreatePost {}
+export class CreatePost {
+  createPostForm = new FormGroup({
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(6), Validators.maxLength(100)],
+    }),
+
+    content: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.maxLength(3000)],
+    }),
+  });
+
+  // getters
+  get title() {
+    return this.createPostForm.controls.title;
+  }
+
+  get content() {
+    return this.createPostForm.controls.content;
+  }
+
+  onFormSubmit() {
+    console.log(this.createPostForm.value);
+  }
+}
