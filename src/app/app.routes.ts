a86cba26a84a461712846b60161a1b4e9ed74e8a
alexc83 <alex.cruce@gmail.com>
2026-04-05 00:44:33 -0500

    feat: add create-post component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CreatePost } from './features/post/pages/create-post/create-post';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'create-post',
+        component: CreatePost
+    }
+];
---
+++ src/app/features/post/pages/create-post/create-post.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Button } from "../../../../shared/button/button";
+
+@Component({
+  selector: 'app-create-post',
+  imports: [Button],
+  templateUrl: './create-post.html',
+  styleUrl: './create-post.css',
+})
+export class CreatePost {}
