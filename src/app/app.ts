a33b3441040433ef0f03de8ae05c2e2bcdae5b76
alexc83 <alex.cruce@gmail.com>
2026-04-05 19:34:37 -0500

    build: add enviornment configuration files and create start-dev command for development enviornment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./core/components/navbar/navbar";
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,5 @@ import { Navbar } from "./core/components/navbar/navbar";
 })
 export class App {
   protected readonly title = signal('devpulse');
+  isProd = environment.prod;
 }
