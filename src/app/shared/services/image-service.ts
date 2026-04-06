5f0d189b221dbf488996fc3f46a1594f50a8c609
alexc83 <alex.cruce@gmail.com>
2026-04-06 02:12:08 -0500

    feat: add firebase storage for image urls. Added image url to database data being saved.

---
+++ src/app/shared/services/image-service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { ref, Storage, uploadBytesResumable, UploadTask } from '@angular/fire/storage';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ImageService {
+
+  firebaseStorage = inject(Storage);
+
+  uploadImage(imageName: string, image: File): UploadTask {
+
+    const storageRef =
+      ref(this.firebaseStorage, `images/${imageName}`);
+
+    return uploadBytesResumable(storageRef, image);
+  }
+
+}
